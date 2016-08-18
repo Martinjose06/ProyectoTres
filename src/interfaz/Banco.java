2b0d91190fbc30a524b4e605e6494273c2f42f64
Martin <martin.jose.06@hotmail.com>
2016-08-17 19:10:35 -0500

    Probado y Finalizado

--- src/interfaz/Banco.java
+++ src/interfaz/Banco.java
@@ -5,6 +5,8 @@
  */
 package interfaz;
 
+import java.awt.event.KeyEvent;
+
 /**
  *
  * @author LUCYLEONOR
@@ -27,21 +29,117 @@ public class Banco extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        txtM = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        lblS = new javax.swing.JLabel();
+        cmdCalcular = new javax.swing.JButton();
+        cmdRestaurar = new javax.swing.JButton();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Banco");
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
+        jLabel1.setText("Banco");
+        jPanel2.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 10, -1, -1));
+
+        jLabel2.setFont(new java.awt.Font("Times New Roman", 0, 13)); // NOI18N
+        jLabel2.setText("Monto Ahorrado");
+        jPanel2.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 70, -1, -1));
+
+        txtM.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtMActionPerformed(evt);
+            }
+        });
+        txtM.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                txtMKeyPressed(evt);
+            }
+        });
+        jPanel2.add(txtM, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 70, 80, -1));
+
+        jLabel3.setFont(new java.awt.Font("Times New Roman", 0, 13)); // NOI18N
+        jLabel3.setText("Saldo Final");
+        jPanel2.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 140, -1, -1));
+
+        lblS.setFont(new java.awt.Font("Times New Roman", 0, 13)); // NOI18N
+        lblS.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblS.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        jPanel2.add(lblS, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 150, 120, 40));
+
+        cmdCalcular.setText("Calcular");
+        cmdCalcular.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmdCalcularActionPerformed(evt);
+            }
+        });
+        jPanel2.add(cmdCalcular, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 250, -1, -1));
+
+        cmdRestaurar.setText("Restaurar");
+        cmdRestaurar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmdRestaurarActionPerformed(evt);
+            }
+        });
+        jPanel2.add(cmdRestaurar, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 250, -1, -1));
+
+        getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 400, 300));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void txtMActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtMActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtMActionPerformed
+
+    private void cmdCalcularActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdCalcularActionPerformed
+
+        String r;
+        double m,t;
+        
+        m = Double.parseDouble(txtM.getText());
+        
+        t = m * 1.015;
+        
+        r = String.valueOf(t);
+        
+        lblS.setText(r);
+    }//GEN-LAST:event_cmdCalcularActionPerformed
+
+    private void cmdRestaurarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdRestaurarActionPerformed
+
+        txtM.setText("");
+        lblS.setText("");
+        txtM.requestFocusInWindow();
+    }//GEN-LAST:event_cmdRestaurarActionPerformed
+
+    private void txtMKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtMKeyPressed
+
+        if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
+        
+        String r;
+        double m,t;
+        
+        m = Double.parseDouble(txtM.getText());
+        
+        t = m * 1.015;
+        
+        r = String.valueOf(t);
+        
+        lblS.setText(r);
+            
+        }
+        
+    }//GEN-LAST:event_txtMKeyPressed
 
     /**
      * @param args the command line arguments
@@ -80,5 +178,14 @@ public class Banco extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton cmdCalcular;
+    private javax.swing.JButton cmdRestaurar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JLabel lblS;
+    private javax.swing.JTextField txtM;
     // End of variables declaration//GEN-END:variables
 }
